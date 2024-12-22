c22976864103b3acd9e99a2d484bea5c60e40a02
troubleman96 <itslugenge96@gmail.com>
2024-12-22 16:40:43 +0300

    first commit

---
+++ Group_MultilevelInheritance01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Person
+{
+    public void Speak()
+    {
+        Console.WriteLine("Person speaks");
+    }
+}
+
+public class Employee : Person
+{
+    public void Work()
+    {
+        Console.WriteLine("The Employee is working...");
+    }
+}
+
+public class Manager : Employee
+{
+    public void Manage()
+    {
+        Console.WriteLine("The manager manages the team for easy coordination");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Manager CrdbManager = new Manager();
+        CrdbManager.Speak();
+        CrdbManager.Work();
+        CrdbManager.Manage(); 
+    }
+}
+        
